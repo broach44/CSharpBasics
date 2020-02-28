36cfa8ecfb285ecaa77c3baa3c1086c71a621d0d
Crystal Broach <crysland@gmail.com>
2020-02-27 22:20:55 -0600

    Add project files.

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Basics!");
+            Console.WriteLine(@"
+
+    *************
+    Calculator
+    *************
+
+             ");
+
+            Console.WriteLine("Please enter some numbers you would like to multiply separated by a comma (NO SPACES!):");
+            var numberStringInput = Console.ReadLine();
+     
+            string[] numberStringList = numberStringInput.Split(",");
+
+            var finalOutput = 0;
+
+            foreach (var numberString in numberStringList)
+            {
+                var result = int.Parse(numberString);
+                if (finalOutput == 0)
+                {
+                    finalOutput = result;
+                }
+                else
+                {
+                finalOutput *= result;
+                }
+                Console.WriteLine(numberString);
+            }
+            Console.WriteLine($"The final result is:  {finalOutput}");
+            Console.ReadLine();
+        }
+    }
+}
